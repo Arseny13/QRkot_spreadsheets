51f6e027dcb78d475077eeac69a4dd9827f1406b
Арсений Изимов <arsenyxiii@gmail.com>
2023-07-26 15:19:55 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# QRkot_spreadseets
+# отчёт в Google Sheets для QRKot
+
+<h4>Автор:</h4>
+
+**Изимов Арсений**  - студент Яндекс.Практикума Когорта 16+
+https://github.com/Arseny13
+
+
+
+<h2>Задание</h2>
+Продолжение проекта https://github.com/Arseny13/cat_charity_fund
+
+Добавьте в приложение QRKot возможность формирования отчёта в гугл-таблице. В таблице должны быть закрытые проекты, отсортированные по скорости сбора средств — от тех, что закрылись быстрее всего, до тех, что долго собирали нужную сумму.
+
+
+<h2>Используемые технологии</h2>
+
+- Python 3.9
+- FastAPI v.0.78.0
+- SQLite
+- SQLAlchemy
+- Alembic
+- Google API
+
+<h2>Как использовать</h2>
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone git@github.com:Arseny13/QRkot_spreadsheets.git
+```
+
+```
+cd QRkot_spreadsheets
+```
+
+Cоздать и активировать виртуальное окружение:
+
+```
+python3 -m venv venv
+```
+
+* Если у вас Linux/macOS
+
+    ```
+    source venv/bin/activate
+    ```
+* Если у вас windows
+
+    ```
+    source venv/scripts/activate
+    ```
+
+
+Установить зависимости из файла requirements.txt:
+
+```
+python3 -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+Заполнить файл .env по аналогу .env.example
+
+Установить alembic и выполнить миграции
+
+```
+alembic init --template async alembic 
+alembic revision --autogenerate -m "Add table reservation"
+alembic upgrade head
+```
+
+Запустить приложение 
+
+```
+uvicorn app.main:app --reload
+```
---
+++ app/schemas/charity_project.py
@@ -0,0 +1,64 @@
+from typing import Optional
+from datetime import datetime
+
+from pydantic import Field, validator, PositiveInt
+
+from .base import CommonBase
+
+
+class CharityProjectBase(CommonBase):
+    """Базовый класс схемы, от которого наследуем схемы для проекта."""
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str] = Field(None, min_length=1)
+
+    @validator('name')
+    def name_not_empty(cls, value: str):
+        if not value:
+            raise ValueError('Не может быть пустым')
+        return value
+
+    @validator('description')
+    def description_not_empty(cls, value: str):
+        if not value:
+            raise ValueError('Не может быть пустым')
+        return value
+
+
+class CharityProjectCreate(CharityProjectBase):
+    name: str = Field(..., min_length=1, max_length=100)
+    description: str = Field(..., min_length=1)
+    full_amount: PositiveInt
+
+
+class CharityProjectUpdate(CharityProjectBase):
+
+    @validator('fully_invested')
+    def fully_invested_not_edit(cls, value: bool):
+        if value:
+            raise ValueError('Закрытый проект нельзя редактировать')
+        return value
+
+    @validator('invested_amount')
+    def invested_amount_not_edit(cls, value: int):
+        if value or value == 0:
+            raise ValueError('invested_amount нельзя редактировать')
+        return value
+
+    @validator('create_date')
+    def create_date_exit(cls, value: datetime):
+        if value:
+            raise ValueError('Нельзя редактировать')
+        return value
+
+    @validator('close_date')
+    def close_date_exit(cls, value: datetime):
+        if value:
+            raise ValueError('Нельзя редактировать')
+        return value
+
+
+class CharityProjectDB(CharityProjectBase):
+    id: int
+
+    class Config:
+        orm_mode = True
